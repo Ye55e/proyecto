992ef8bbab43e05e64be079fb10b7fa73f29dd3e
Ye55e <yessena,canchig8531@utc.edu.ec>
2025-05-24 00:59:27 -0500

    formularios producto categoria

--- Aplicaciones/Ajditec/urls.py
+++ Aplicaciones/Ajditec/urls.py
@@ -4,6 +4,12 @@ from . import views
 urlpatterns = [
     path('', views.inicio, name='inicio'),
     path('pago/', views.pago_view, name='pago'),
-    path('carrito/', views.carrito_view, name='carrito')
+    path('carrito/', views.carrito_view, name='carrito'),
+    
+    #Productos
+    path('nuevoProducto/', views.nuevoProducto, name='nuevoProducto'),
+    
+    #Categorias
+    path('nuevoCategoria/', views.nuevoCategoria, name='nuevoCategoria')
 ]
 
--- Aplicaciones/Ajditec/views.py
+++ Aplicaciones/Ajditec/views.py
@@ -4,8 +4,14 @@ from django.shortcuts import render
 def inicio(request):
     return render(request, 'inicio.html')
 
+def nuevoProducto(request):
+    return render(request, 'nuevoProducto.html')
+
 def pago_view(request):
     return render(request, 'pago.html')
 
 def carrito_view(request):
     return render(request, 'carrito.html')
+
+def nuevoCategoria(request):
+    return render(request, 'nuevoCategoria.html')
