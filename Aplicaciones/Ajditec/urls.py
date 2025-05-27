ca7ea8375db557877262f1016034085b4fb199eb
Ye55e <yessena,canchig8531@utc.edu.ec>
2025-05-27 11:16:37 -0500

    crud categoria

--- Aplicaciones/Ajditec/urls.py
+++ Aplicaciones/Ajditec/urls.py
@@ -10,6 +10,13 @@ urlpatterns = [
     path('nuevoProducto/', views.nuevoProducto, name='nuevoProducto'),
     
     #Categorias
-    path('nuevoCategoria/', views.nuevoCategoria, name='nuevoCategoria')
+    path('nuevoCategoria/', views.nuevoCategoria, name='nuevoCategoria'),
+    path('listadoCategoria/',views.listadoCategoria, name='listadoCategoria'),
+    path('guardarCategoria/',views.guardarCategoria, name='guardarCategoria'),
+    path('eliminarCategoria/<id_categoria>', views.eliminarCategoria, name='eliminarCategoria'),
+    path('editarCategoria/<id_categoria>',views.editarCategoria, name='editarCategoria'),
+    path('procesarEdicionCategoria/',views.procesarEdicionCategoria, name='procesarEdicionCategoria'),
+    path('ver-bd/', views.mostrar_base_datos),
+
 ]
 
--- Aplicaciones/Ajditec/views.py
+++ Aplicaciones/Ajditec/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib import messages
+from .models import Producto, Categoria 
 
 # Create your views here.
 def inicio(request):
@@ -15,3 +17,49 @@ def carrito_view(request):
 
 def nuevoCategoria(request):
     return render(request, 'nuevoCategoria.html')
+
+#Categoria 
+def nuevoCategoria(request):
+    categoria = Categoria.objects.all()
+    return render (request, 'nuevoCategoria.html',{
+        'categoria':categoria
+    })
+
+def listadoCategoria(request):
+    categoriaBdd = Categoria.objects.all()
+    return render(request, 'listadoCategoria.html', 
+                  {'categoria':categoriaBdd})
+
+def guardarCategoria(request):
+    tipo_cat = request.POST['tipo_cat']
+
+    nuevoCategoria = Categoria.objects.create(
+        tipo_cat = tipo_cat,
+    )
+
+    messages.success(request,"Se ha guardado la categoria")
+    return redirect ('/listadoCategoria')
+
+def eliminarCategoria(request, id_categoria):
+    categoriaELiminar = get_object_or_404(Categoria, id_cat=id_categoria)
+    categoriaELiminar.delete()
+    messages.success(request,"Categoria Eliminada")
+    return redirect('listadoCategoria')
+
+def editarCategoria(request, id_categoria):
+    categoriaEditar = Categoria.objects.get(id_cat = id_categoria)
+    return render(request,'editarCategoria.html', {'categoria':categoriaEditar })
+
+def procesarEdicionCategoria(request):
+    categoria=Categoria.objects.get(id_cat = request.POST['id_cat'])
+    categoria.tipo_cat=request.POST['tipo_cat']
+
+    categoria.save()
+    messages.success(request,"Categoria actualizada con exito")
+    return redirect('/listadoCategoria')
+
+from django.http import HttpResponse
+from django.db import connection
+
+def mostrar_base_datos(request):
+    return HttpResponse(f"Base de datos en uso: {connection.settings_dict['NAME']}")
