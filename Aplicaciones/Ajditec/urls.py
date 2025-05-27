313aa5a4c6ab45604789307192cd00d5544d7b19
Masiel04 <adriana.carua7617@utc.edu.ec>
2025-05-27 15:03:05 -0500

    cambios product

--- Aplicaciones/Ajditec/urls.py
+++ Aplicaciones/Ajditec/urls.py
@@ -9,6 +9,11 @@ urlpatterns = [
     
     #Productos
     path('nuevoProducto/', views.nuevoProducto, name='nuevoProducto'),
+    path('listadoProducto/',views.listadoProducto, name='listadoProducto'),
+    path('guardarProducto/',views.guardarProducto, name='guardarProducto'),
+    path('eliminarProducto/<id_prod>', views.eliminarProducto, name='eliminarProducto'),
+    path('editarProducto/<id_prod>',views.editarProducto, name='editarProducto'),
+    path('procesarEdicionProducto/',views.procesarEdicionProducto, name='procesarEdicionProducto'),
     
     #Categorias
     path('nuevoCategoria/', views.nuevoCategoria, name='nuevoCategoria'),
@@ -17,7 +22,7 @@ urlpatterns = [
     path('eliminarCategoria/<id_categoria>', views.eliminarCategoria, name='eliminarCategoria'),
     path('editarCategoria/<id_categoria>',views.editarCategoria, name='editarCategoria'),
     path('procesarEdicionCategoria/',views.procesarEdicionCategoria, name='procesarEdicionCategoria'),
-    path('ver-bd/', views.mostrar_base_datos),
+   
 
 
 ]
